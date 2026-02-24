622a25bc02fc1b5cf693358400ab7c30fcc56ced
EversonPrieto <eversonlemosprieto@outlook.com>
2026-02-24 10:43:27 -0300

    feat(auth): add JWT login and protected me route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,16 +1,26 @@
-// backend/src/auth/auth.controller.ts
-
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup') // Rota: POST http://localhost:3000/auth/signup
+  @Post('signup')
   signUp(@Body() dto: CreateUserDto) {
-    // O @Body() pega o corpo da requisição e o Nest valida usando o DTO.
     return this.authService.signUp(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getProfile(@Req() req: any) {
+    return req.user;
   }
 }
